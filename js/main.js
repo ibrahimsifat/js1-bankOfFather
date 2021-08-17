dcd8963a41e39d71bde1c8a6c82d61fd56dc55ac
ibrahimsifat <ibsifat24@gmail.com>
2021-08-17 14:56:46 +0300

    this is git

--- js/main.js
+++ js/main.js
@@ -1,64 +1,46 @@
-//selection
-let dipositInput = document.getElementById('depositeInput')
-
-let dipositText = document.getElementById('depositText')
-
-let dipositBtn = document.getElementById('depositBtn')
-    // addevenlitchener of deposite
-dipositBtn.addEventListener('click', function() {
-    // collect input value
-    let dipositInputString = dipositInput.value
-    let dipositInputAmount = parseFloat(dipositInputString)
-
-    // deposite text
-    let dipositTextString = dipositText.innerText
-    let dipositTextAmount = parseFloat(dipositTextString)
-        //addition 
-    let newdipositAmount = dipositTextAmount + dipositInputAmount
-        // sectct value 
-    dipositText.innerText = newdipositAmount
-        //clear input
-    dipositInput.value = ''
-
-    // add deposit value on blance
-    let balanceText = document.getElementById('balanceText')
-    let balanceTextString = balanceText.innerText
-        //convarting string to number
-    let balanceTextAmount = parseFloat(balanceTextString)
-        //adding blance and diposit
-    let newBalanceValue = balanceTextAmount + dipositInputAmount
-        //displaying
-    balanceText.innerText = newBalanceValue
-})
-
-
-//withdraw btn add event
-//select add
+// selection 
+let depositInput = document.getElementById('depositInput')
+let depositText = document.getElementById('depositText')
+let depositBtn = document.getElementById('depositBtn')
+    // withdraw 
 let withdrawInput = document.getElementById('withdrawInput')
 let withdrawText = document.getElementById('withdrawText')
 let withdrawBtn = document.getElementById('withdrawBtn')
-    // add evevt on the btn
+    // balance 
+let balanceText = document.getElementById('balanceText')
+    // get input to floting number
+function getInput(inputId) {
+    let input = document.getElementById(inputId)
+    let inputString = input.value
+    let inputAmount = parseFloat(inputString)
+    return inputAmount
+}
+// get text number function 
+function getText(textId) {
+    let text = document.getElementById(textId)
+    let textString = text.innerText
+    let textAmount = parseFloat(textString)
+    return textAmount
+}
+//deposit section add event handle
+depositBtn.addEventListener('click', function() {
+        depositText.innerText = getInput('depositInput') + getText('depositText')
+            //balence add
+        balanceText.innerText = getBalence('balanceText') + getInput('depositInput')
+            // clear input 
+        depositInput.value = ''
+    })
+    //withdraw section add event handle
 withdrawBtn.addEventListener('click', function() {
-    // get value of input
-    let withdrawInputString = withdrawInput.value;
-    let withdrawInputAmount = parseFloat(withdrawInputString);
-    // get value of text
-    let withdrawTextString = withdrawText.innerText;
-    let withdrawTextAmount = parseFloat(withdrawTextString)
-        // adding display value 
-    let newwithdrawValue = withdrawInputAmount + withdrawTextAmount
-        // display value 
-    withdrawText.innerText = newwithdrawValue
-        //clear input
-    withdrawInput.value = ''
-        // decreasing blancing value
-
-    let balanceText = document.getElementById('balanceText');
-    // get innertext
-    let balanceTextString = balanceText.innerText;
-    // convarting string to num
-    let balanceTextAmount = parseFloat(balanceTextString)
-    let newBalanceValue = balanceTextAmount - withdrawInputAmount
-        //displaying value
-    balanceText.innerText = newBalanceValue
-})
+        withdrawText.innerText = getInput('withdrawInput') + getText('withdrawText')
+        balanceText.innerText = getBalence('balanceText') - getInput('withdrawInput')
+            // clear input 
+        withdrawInput.value = ''
+    })
+    //balence function
+function getBalence(balenceId, ) {
+    let balenceValue = document.getElementById(balenceId)
+    let balenceString = balenceValue.innerText
+    let balenceAmount = parseFloat(balenceString)
+    return balenceAmount
+}
